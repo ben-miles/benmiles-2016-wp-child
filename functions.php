dab04f088c2f305a47c18764d9465dd9c9790e5a
Ben <bcgm3@bcgm3.com>
2016-10-18 22:44:39 -0400

    Dynamic path to custom JS in footer
    Added PHP to derive relative path to custom footer JS from current
    request URI.

--- functions.php
+++ functions.php
@@ -1,5 +1,25 @@
-<?php function add_this_script_footer(){ ?>
-<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js'></script>
-<script type='text/javascript' src='http://bcgm3.com/wp-content/themes/benmiles/js/footer.js'></script>
-<?php }
-add_action('wp_footer', 'add_this_script_footer', 99); ?>
+<?php
+
+function add_this_script_footer() {
+
+  // Change relative path according to current address
+  $count = count( explode( "/", $_SERVER['REQUEST_URI'] ) );
+
+  if( $count <= 2 ) {
+    // Home
+    $pathToCustomJS = "wp-content/themes/benmiles/js/footer.js";
+  } elseif( $count == 3 ) {
+    // One subdirectory
+    $pathToCustomJS = "../wp-content/themes/benmiles/js/footer.js";
+  } else {
+    // Two+ Subdirectories
+    $pathToCustomJS = "../../wp-content/themes/benmiles/js/footer.js";
+  }
+
+  echo "<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js'></script>";
+  echo "<script type='text/javascript' src='$pathToCustomJS'></script>";
+}
+
+add_action('wp_footer', 'add_this_script_footer', 99);
+
+?>
